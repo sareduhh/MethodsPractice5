1ffd894bc1dc20be3401c6bf1b173d2cff911f98
sareduhh <sarahe.kaminski@gmail.com>
2016-09-14 18:47:49 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPractice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a method named "Double" to calculate and return an integer number doubled. For example. Double(7) should return 14.
+            int num = 6;
+            Console.WriteLine(Double(num));
+        }
+        static int Double(int num)
+        {
+            int num2 = num * 2;
+            return num2;
+          
+        }
+        
+    }
+}
